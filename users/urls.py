ad57a919ecf5825b1f757639a492bc9d642557aa
alexhr1 <135211383+alexhr1@users.noreply.github.com>
2025-05-19 01:58:53 +0300

    Fix logout to use POST instead of GET

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import login_view, register_view, home_view
 from . import views # Импортираме views от текущото приложение
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home_view, name='home'),  # Начална страница
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
     path('about/', views.about_view, name='about'),  # 🟢 Добави това
     path('dashboard/', views.dashboard_view, name='dashboard'),
