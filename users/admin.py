913616d6223af2b77a1fb29d2249649637552432
Alexander Hristov <alexander.hristov@stingpharma.com>
2025-05-19 11:59:09 +0300

    Added new models, details, and database

--- users/admin.py
+++ users/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import AppUser
 
+
 @admin.register(AppUser)
-class AppUserAdmin(admin.ModelAdmin):
-    list_display = (
-        'username', 'email', 'is_staff',
-        'is_driver', 'is_transport_manager',
-        'is_logistics', 'is_logistics_manager',
-        'is_warehouseman',
+class CustomUserAdmin(UserAdmin):
+    model = AppUser
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_logistics', 'is_driver', 'is_warehouseman')
+    list_filter = ('is_logistics', 'is_driver', 'is_warehouseman', 'is_logistics_manager', 'is_transport_manager', 'is_dealer')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Допълнителна информация', {
+            'fields': ('phone', 'position', 'created_by_admin', 'notes',
+                       'is_logistics', 'is_logistics_manager', 'is_driver',
+                       'is_transport_manager', 'is_warehouseman', 'is_dealer'),
+        }),
     )
-    list_filter = (
-        'is_staff',
-        'is_driver',
-        'is_transport_manager',
-        'is_logistics',
-        'is_logistics_manager',
-        'is_warehouseman',
-    )
+
     search_fields = ('username', 'email')
     ordering = ('username',)
