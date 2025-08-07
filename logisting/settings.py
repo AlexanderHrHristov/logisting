4136e52bea7eed89353ab70e7d63945b5cb42b03
alexhr1 <135211383+alexhr1@users.noreply.github.com>
2025-08-07 17:06:05 +0300

    Fixed dotenv Error

--- logisting/settings.py
+++ logisting/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
 from dotenv import load_dotenv
+import os
 from pathlib import Path
 
 # Load environment variables from .env file
